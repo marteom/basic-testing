99885a2d18404dc8914b9b5f63eccea3eed72255
marteom <marteom@gmail.com>
2024-02-10 23:04:32 +0300

    feat: fill tests for 02-table-tests

--- src/02-table-tests/index.test.ts
+++ src/02-table-tests/index.test.ts
@@ -1,17 +1,26 @@
-// Uncomment the code below and write your tests
-/* import {  simpleCalculator, Action } from './index';
+import {  simpleCalculator, Action } from './index';
+
+interface TestData {
+  a: number, 
+  b: number,
+  action: Action,
+  expected: number
+}
 
 const testCases = [
     { a: 1, b: 2, action: Action.Add, expected: 3 },
-    { a: 2, b: 2, action: Action.Add, expected: 4 },
-    { a: 3, b: 2, action: Action.Add, expected: 5 },
-    // continue cases for other actions    
-]; */
+    { a: 2, b: 2, action: Action.Divide, expected: 1 },
+    { a: 3, b: 2, action: Action.Exponentiate, expected: 9 },
+    { a: 4, b: 4, action: Action.Multiply, expected: 16 },
+    { a: 3, b: 2, action: Action.Subtract, expected: 1 },
+];
 
 describe('simpleCalculator', () => {
-  // This test case is just to run this test suite, remove it when you write your own tests
-  test('should blah-blah', () => {
-    expect(true).toBe(true);
+  test.each(testCases)('calculate(%i, %i) should return $expected', ({a, b, action, expected}: TestData) => {
+    expect(simpleCalculator({
+      a,
+      b,
+      action
+    })).toBe(expected);
   });
-  // Consider to use Jest table tests API to test all cases above
 });
