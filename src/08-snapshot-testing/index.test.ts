309904471b8a2d29fb3061e3ff561eaa0eeb99cb
marteom <marteom@gmail.com>
2024-02-11 21:30:21 +0300

    feat: fill tests for 08-snapshots-testing

--- src/08-snapshot-testing/index.test.ts
+++ src/08-snapshot-testing/index.test.ts
@@ -1,14 +1,25 @@
-// Uncomment the code below and write your tests
-// import { generateLinkedList } from './index';
+import { generateLinkedList } from './index';
 
 describe('generateLinkedList', () => {
-  // Check match by expect(...).toStrictEqual(...)
   test('should generate linked list from values 1', () => {
-    // Write your test here
+    const elements = [1];
+    const linksList = {
+      value: 1,
+      next: {
+        value: null,
+        next: null,
+      },
+    };
+
+    const result = generateLinkedList(elements);
+
+    expect(result).toStrictEqual(linksList);
   });
 
-  // Check match by comparison with snapshot
   test('should generate linked list from values 2', () => {
-    // Write your test here
+    const elements = [2];
+    const result = generateLinkedList(elements);
+
+    expect(result).toMatchSnapshot();
   });
 });
